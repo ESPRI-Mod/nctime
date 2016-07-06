d5a7af355e73981ebc2d58fb2de7dae7d1a1f404
Guillaume <glipsl@ipsl.jussieu.fr>
2016-07-06 15:14:10 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='timeaxis',
-      version='3.5',
+      version='3.6',
       description='Check/Rewrite time axis of MIP NetCDF files.',
       author='Levavasseur Guillaume',
       author_email='glipsl@ipsl.jussieu.fr',
