fc967062d5ff5780ab96a7ca252d6c212a4c0c38
Guillaume <glipsl@ipsl.jussieu.fr>
2016-12-23 11:52:59 +0100

    bugfix update

--- nctime/nctime.py
+++ nctime/nctime.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from utils.utils import MultilineFormatter, init_logging, directory_checker, path_switcher
 
 # Program version
-__version__ = 'v{0} {1}'.format('3.8.8', datetime(year=2016, month=12, day=23).strftime("%Y-%d-%m"))
+__version__ = 'v{0} {1}'.format('3.8.9', datetime(year=2016, month=12, day=23).strftime("%Y-%d-%m"))
 
 
 def get_args():
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nctime',
-      version='3.8.8',
+      version='3.8.9',
       description='Diagnoses NetCDF time axis.',
       author='Levavasseur Guillaume',
       author_email='glipsl@ipsl.jussieu.fr',
