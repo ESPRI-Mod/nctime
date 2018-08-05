Fix climatology bounds calculation

nctime/axis/handler.py
@@ -129,14 +129,22 @@ class File(object):
                                                   calendar=self.calendar,
                                                   start=input_start_timestamp,
                                                   end=input_end_timestamp)
-        if self.is_climatology:
-            # Climatology is on the same year in the middle of the period
-            year_diff = dates[1].year - dates[0].year
-            dates[0] = dates[0].replace(year=dates[0].year + year_diff / 2)
-            dates[1] = dates[1].replace(year=dates[0].year)
-            dates[2] = dates[2].replace(year=dates[0].year)
         dates_num = trunc(date2num(dates, units=self.funits, calendar=self.calendar), NDECIMALS)
-        if not self.is_instant and (self.frequency in AVERAGE_CORRECTION_FREQ or self.frequency in CLIMATOLOGY_FREQ):
+        if self.is_climatology:
+            # Get climatology offset to start in the middle of the interval
+            year_diff = dates[1].year - dates[0].year
+            start_clim_date = dates[0].replace(year=dates[0].year + year_diff / 2)
+            start_clim_num = trunc(date2num(start_clim_date, units=self.funits, calendar=self.calendar), NDECIMALS)
+            # Apply time offset corresponding to the climatology:
+            if self.frequency in ['monC', 'monClim']:
+                self.clim_diff = (start_clim_num - dates_num[0], dates_num[1] - 11 - start_clim_num)
+            elif self.frequency == '1hrCM':
+                self.clim_diff = (start_clim_num - dates_num[0], dates_num[1] - 23.5 - start_clim_num)
+            else:
+                raise InvalidClimatologyFrequency(self.frequency)
+            dates_num[0] += self.clim_diff[0]
+            dates_num[1] -= self.clim_diff[1]
+        elif not self.is_instant and self.frequency in AVERAGE_CORRECTION_FREQ:
             # Apply time offset for non-instant time axis:
             dates_num += 0.5
         self.start_axis = dates_num[0]
@@ -183,7 +191,10 @@ class File(object):
                              step=self.step)
         num_axis = self.check_axis_length(num_axis)
         num_axis_bnds_inf, num_axis_bnds_sup = num_axis, copy(num_axis)
-        if not self.is_instant:
+        if self.is_climatology:
+            num_axis_bnds_inf -= self.clim_diff[0]
+            num_axis_bnds_sup += self.clim_diff[1]
+        elif not self.is_instant:
             num_axis_bnds_inf -= 0.5 * self.step
             num_axis_bnds_sup += 0.5 * self.step
         num_axis_bnds = np.column_stack((num_axis_bnds_inf, num_axis_bnds_sup))