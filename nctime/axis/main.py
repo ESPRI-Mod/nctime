00359b645c13d0c87668581fe65901bc58b439ad
Guillaume <glipsl@ipsl.fr>
2018-09-19 13:17:08 +0200

    Bugfix in calendar correction

--- nctime/axis/main.py
+++ nctime/axis/main.py
@@ -99,10 +99,7 @@ def process(ffp):
             fh.nc_att_overwrite('calendar', variable='time', data=pctx.ref_calendar)
             # Rewrite time boundaries if needed
             if fh.has_bounds:
-                if fh.is_climatology:
-                    fh.nc_var_overwrite('climatology_bounds', fh.time_bounds_rebuilt)
-                else:
-                    fh.nc_var_overwrite('time_bounds', fh.time_bounds_rebuilt)
+                fh.nc_var_overwrite(fh.tbnds, fh.time_bounds_rebuilt)
         # Diagnostic display
         msg = """{}
         Units: {} [ref = {}]
